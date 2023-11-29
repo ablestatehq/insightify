21045fa5c65511526200fb2080a3314fbadaa16b
wasswaenockmale <enockmale97@gmail.com>
2023-11-29 11:07:34 +0300

    skeleton

---
+++ src/screens/OpportunityStack/OpportunityList/OpportunityList.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const OpportunityList = () => {
+  return (
+    <View>
+      <Text>OpportunityList</Text>
+    </View>
+  )
+}
+
+export default OpportunityList
+
+const styles = StyleSheet.create({})
