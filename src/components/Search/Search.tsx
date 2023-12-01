5894506f223a9c11c2d1151e5ad225678337b4fc
wasswaenockmale <enockmale97@gmail.com>
2023-12-01 12:33:46 +0300

    Search bar component

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Ionicons } from '@expo/vector-icons'
+import { COLOR, FONTSIZE } from '../../constants/contants'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+
+const Search = () => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>
+            Discover Limitless <Text style={styles.oppStyle}>Opportunities.</Text>
+          </Text>
+        </View>
+      </View>
+      {/* <View style={styles.searchMain}>
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder='Search...'
+          />
+          <Ionicons name="search-outline" size={24} color="black" />
+        </View>
+      </View> */}
+
+      {/* Add the tags  */}
+      <View>
+
+      </View>
+    </View>
+  )
+}
+
+export default Search
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+  },
+  searchContainer: {
+    width: '100%',
+    borderRadius: 20,
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    backgroundColor: COLOR.B_50,
+    alignItems: 'center',
+    elevation:1,
+  },
+  textInput: {
+    flex: 1,
+    padding: 5,
+    fontFamily: 'ComfortaaBold'
+  },
+  text: {
+    fontSize: FONTSIZE.HEADING_4,
+    fontFamily: 'RalewayBold',
+
+  },
+  textContainer: {
+  },
+  searchMain: {
+    // flex: 1,
+    justifyContent: 'center',
+    marginTop: 10
+  },
+  oppStyle: {
+    // color:COLOR.ORANGE_300
+  }
+})
