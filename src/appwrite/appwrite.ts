1ce99e955c01c6b3de606504ad062b3149ef0256
wasswaenockmale <enockmale97@gmail.com>
2023-12-01 12:41:56 +0300

    login and create account services

--- src/appwrite/appwrite.ts
+++ src/appwrite/appwrite.ts
@@ -1,6 +1,6 @@
-import { Client, Databases, ID, Query } from "appwrite";
 import { environments } from "../constants/environments";
-import { FeedbackObject, TalentSubmissionForm, userModal } from "../utils/types";
+import { Client, Databases, ID, Query, Account } from "appwrite";
+import { FeedbackObject, OpportunitiesFormType, TalentSubmissionForm, userModal } from "../utils/types";
 
 export const appwriteClient = new Client();
 
@@ -9,8 +9,9 @@ const {
   APPWRITE_PROJECT_ENDPOINT,
 } = environments;
  
- class AppwriteService {
+class AppwriteService {
   databases: Databases;
+  account: Account;
 
   constructor(){
     appwriteClient
@@ -18,8 +19,41 @@ const {
       .setProject(APPWRITE_PROJECT_ID)
       
     this.databases = new Databases(appwriteClient);
+    this.account = new Account(appwriteClient);
+  }
+  // Account
+  async createAccountWithEmail(email:string, password:string) {
+    try {
+      const accountResponse = await this.account.create(ID.unique(),email,password)
+      return accountResponse;
+    } catch (error) {
+    }
   }
 
+  async createAccountWithAuth2() {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+  async loginWithEmailAndPassword(email:string, password: string) {
+    try {
+      const loginResponse = await this.account.createEmailSession(email, password)
+      return loginResponse
+    } catch (error) {
+      
+    }
+  }
+
+  async logOut() {
+    try {
+      await this.account.deleteSession('current')
+    } catch (error) {
+      
+    }
+  }
+  // Data functions, fetch and store
   async getDBData(databaseID:string, collectionID:string) {
     try {
       return (await this.databases.listDocuments(databaseID, collectionID,[Query.limit(100), Query.offset(0)])).documents
@@ -27,8 +61,15 @@ const {
       console.log('getDBData -- ', error.message);
     }
   }
+  async getDocument(databaseID:string, collectionID:string, documentId:string) {
+    try {
+      return (await this.databases.getDocument(databaseID, collectionID,documentId)).documents
+    } catch (error:any) {
+      console.log('getDBData -- ', error.message);
+    }
+  }
    
-  async storeDBdata(databaseID: string, collectionID: string, data:userModal | TalentSubmissionForm | FeedbackObject) {
+  async storeDBdata(databaseID: string, collectionID: string, data:userModal | TalentSubmissionForm | FeedbackObject | OpportunitiesFormType) {
     try {
       return this.databases.createDocument(databaseID, collectionID, ID.unique(),data)
     } catch (error:any) {
