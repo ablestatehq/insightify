25d048de7a4aac66e8fbb4d1a6be09f0ee6496ca
wasswaenockmale <enockmale97@gmail.com>
2023-12-06 10:39:24 +0300

    feat: able to share opportunities

--- src/screens/OpportunityStack/OpportunityList/OpportunityList.tsx
+++ src/screens/OpportunityStack/OpportunityList/OpportunityList.tsx
@@ -1,14 +1,120 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { COLOR } from '../../../constants/contants'
+import { AppContext } from '../../../helper/context/AppContext'
+import { StyleSheet, View, ScrollView, StatusBar, Text } from 'react-native'
+import { Filter, OpportunityCard, Search, FloatingButton, FilterCard } from '../../../components'
+
 
 const OpportunityList = () => {
+
+  const { opportunities } = useContext(AppContext);
+  const [filteredItems, setFilteredItems] = useState<string[]>([]);
+  const [showCard, setShowCard] = useState<boolean>(false);
+
+  const showFilterCard = () => {
+    setShowCard(!showCard);
+  }
+
+  const filteredOpportunities = opportunities.filter((opp: any) => filteredItems.includes(opp.title));
+
   return (
-    <View>
-      <Text>OpportunityList</Text>
+    <View style={styles.container}>
+      <StatusBar barStyle='dark-content' backgroundColor={COLOR.WHITE} />
+      {/* search section  */}
+      <View style={styles.searchContainer}>
+        <Search />
+        <Filter handlePress={showFilterCard} />
+      </View>
+
+      <View style={styles.opportunityListContainer}>
+        {filteredItems.length > 0 ?
+          filteredOpportunities.length > 0 ?
+            <ScrollView
+              contentContainerStyle={styles.scrolllOpp}
+              showsVerticalScrollIndicator={false}
+            >
+              {filteredOpportunities.map((_, index: number) => (
+                <OpportunityCard
+                  key={index}
+                  location={_.location}
+                  createdAt={_.createdAt}
+                  type={_.type}
+                  link={_.link}
+                  title={_.title}
+                  expireDate={_.expiryDate}
+                  description={_.description}
+                />
+              ))}
+            </ScrollView>
+            : (
+              <View style={styles.noTextStylesContainer}>
+                <Text style={styles.noMatcheStyle}>We Couldn't Find Any Matches</Text>
+                <Text style={styles.noMatcheStyle}>Try Adjusting Your Filters</Text>
+              </View>
+            )
+          :
+          <ScrollView
+            contentContainerStyle={styles.scrolllOpp}
+            showsVerticalScrollIndicator={false}
+          >
+            {opportunities.map((_, index: number) => (
+              <OpportunityCard
+                key={index}
+                location={_.location}
+                createdAt={_.createdAt}
+                type={_.type}
+                link={_.link}
+                title={_.title}
+                expireDate={_.expiryDate}
+                description={_.description}
+              />
+            ))}
+          </ScrollView>
+        }
+        <FloatingButton title='Add Opportunity' />
+        {/* filter card  */}
+        {showCard && <FilterCard
+          handleCardVisibility={showFilterCard}
+          setFilteredItems={setFilteredItems}
+          filteredItems={filteredItems}
+        />}
+      </View>
     </View>
   )
 }
 
 export default OpportunityList
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLOR.WHITE,
+  },
+  opportunityListContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  scrolllOpp: {
+    padding: 5
+  },
+  opportunityHeaderStyles: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    justifyContent: 'space-between',
+    paddingVertical:10
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 15
+  },
+  noMatcheStyle: {
+    fontFamily:'ComfortaaBold'
+  },
+  noTextStylesContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
--- src/screens/OpportunityStack/ShareOpportunity/Share.tsx
+++ src/screens/OpportunityStack/ShareOpportunity/Share.tsx
@@ -1,14 +1,169 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Formik, FormikHelpers } from 'formik'
+import { Ionicons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { ShareSchema } from '../../../utils/validations'
+import DatabaseService from '../../../appwrite/appwrite'
+import { OpportunitiesFormType } from '../../../utils/types'
+import { COLOR, FONTSIZE } from '../../../constants/contants'
+import { environments } from '../../../constants/environments'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import InputText from '../../../components/FomikComponents/InputText/InputText'
+import SubmitButton from '../../../components/FomikComponents/SubmitButton/SubmitButton'
+import { KeyboardAvoidingView, ScrollView, StyleSheet, Text, ToastAndroid, View } from 'react-native'
+import { TagInput } from '../../../components'
 
+const { 
+  APPWRITE_DATABASE_ID,
+  APPWRITE_OPPORTUNITIES_COLLECTION_ID
+} = environments;
 const Share = () => {
+
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+
+  const handleOpportunitySubmission = async (values: any, formikHelpers: FormikHelpers<any>) => {
+    try {
+      const data:OpportunitiesFormType = {
+        ...values
+      }
+      // console.log(data)
+      const response =
+        await DatabaseService.storeDBdata(
+          APPWRITE_DATABASE_ID,
+          APPWRITE_OPPORTUNITIES_COLLECTION_ID,
+          data
+        )
+          .then(response => response)
+          .catch(error => {
+            console.log(error);
+          });
+      if (response) {
+        ToastAndroid.show('Request successfully sent', 5000);
+        formikHelpers.resetForm({
+          values: formInitialValues
+        });
+
+      }
+    } catch (error) {
+      
+    }
+  };
+  
+  const formInitialValues = {
+    link: "",
+    type: "",
+    title: "",
+    location: "",
+    expiryDate: "",
+    description: "",
+    companyName: "",
+    // tag: ""
+  }
+
+  const handleTagAddition = (newTag:string) => {
+    console.log('New tag added:', newTag);
+  };
   return (
-    <View>
-      <Text>Share</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.header}>
+        <Ionicons
+          name="chevron-back"
+          size={30}
+          color="black"
+          onPress={() => {
+            
+            navigation.navigate('Home')
+          }}
+        />
+        <Text
+          style={styles.text}
+        >
+          Opportunity Hub Form
+        </Text>
+        <View />
+      </View >
+      <View style={styles.formContainer}>
+        <Formik
+          validationSchema={ShareSchema}
+          initialValues={formInitialValues}
+          onSubmit={handleOpportunitySubmission}
+        >
+          {({ handleSubmit }) => (
+            <ScrollView
+              showsVerticalScrollIndicator={false}
+              contentContainerStyle={styles.formViewContainer}
+            >
+              <InputText
+                label='Title'
+                fieldName='title'
+                placeholder='Title'
+              />
+              <InputText
+                label='Description'
+                isMultiLine={true}
+                fieldName='description'
+                placeholder='Provide a simple description of the opportunity'
+              />
+              <InputText
+                label='Link'
+                fieldName='link'
+                placeholder='e.g https://www.example.com'
+              />
+              <InputText
+                label='Location'
+                fieldName='location'
+                placeholder='Location'
+              />
+              <InputText
+                label='Company Name'
+                fieldName='companyName'
+                placeholder='Company Name'
+              />
+              <InputText
+                label='Type'
+                fieldName='type'
+                placeholder='Type e.g Coding, Product Manager'
+              />
+              {/* <TagInput
+                onAddTag={handleTagAddition}
+              /> */}
+              <InputText
+                label='Expiry Date'
+                fieldName='expiryDate'
+                placeholder='e.g YYYY-MM-DD'
+              />
+
+              <SubmitButton handleSubmit={() => handleSubmit()} />
+            </ScrollView>
+          )}
+        </Formik>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
-export default Share
+export default Share;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLOR.WHITE
+  },
+  text: {
+    flex: 1,
+    textAlign: 'center',
+    fontFamily: 'RalewayBold',
+    fontSize: FONTSIZE.TITLE_1,
+  },
+  header: {
+    flexDirection: 'row',
+    padding: 5,
+  },
+  formContainer: {
+    flex: 1,
+    padding: 10
+  },
+  formViewContainer: {
+    padding: 5
+  },
+})
