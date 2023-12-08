131e2300c5572b90975282c04f33efe06c0786ca
wasswaenockmale <enockmale97@gmail.com>
2023-12-08 18:56:28 +0300

    More screen

---
+++ src/screens/MoreDrawerScreen/MoreDrawerScreen.tsx
@@ -0,0 +1,192 @@
+import React, { useContext, useState } from 'react'
+import { COLOR, FONTSIZE } from '../../constants/contants'
+import { Entypo, SimpleLineIcons, Ionicons, MaterialIcons, AntDesign, Feather } from '@expo/vector-icons';
+import { Pressable, StyleSheet, Switch, Text, View } from 'react-native'
+import { AppContext } from '../../helper/context/AppContext';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import DatabaseService from '../../appwrite/appwrite';
+
+const MoreDrawerScreen = () => {
+  const { isLoggedIn, setIsLoggedIn } = useContext(AppContext);
+  const [isEnabled, setIsEnabled] = useState(false);
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+
+  const handleLoginLogout = async () => {
+    if (isLoggedIn) {
+      await DatabaseService.logOut();
+      setIsLoggedIn(false);
+    } else {
+      navigation.navigate('Login',{title: ''})
+    }
+  }
+  return (
+    <View style={styles.container}>
+
+      {/* Account  */}
+      {/* <Text style={styles.textHeading}>Account</Text>
+      <View style={styles.contentContainer}>
+        <View style={styles.itemContainer}>
+          <View style={styles.iconContainer}>
+            <Ionicons name="person-add-outline" size={24} color="black" />
+            <Text>Edit profile</Text>
+          </View>
+          <Entypo name="chevron-thin-right" size={15} color={COLOR.B_300} />
+        </View>
+        <View style={styles.itemContainer}>
+          <View style={styles.iconContainer}>
+            <Feather name="lock" size={24} color="black" />
+            <Text>Change password</Text>
+          </View>
+          <Entypo name="chevron-thin-right" size={15} color={COLOR.B_300} />
+        </View>
+      </View> */}
+
+      {/* Notifications  */}
+      <Text style={styles.textHeading}>Notification</Text>
+      <View style={styles.contentContainer}>
+        <View style={styles.itemContainer}>
+          <View style={styles.iconContainer}>
+            <Ionicons name="ios-notifications-outline" size={24} color={COLOR.B_300} />
+            <Text style={styles.text}>Push notifications</Text>
+          </View>
+          <Switch
+            trackColor={{ false: `${COLOR.B_100}`, true: `${COLOR.ORANGE_100}`}}
+            thumbColor={isEnabled ? `${COLOR.ORANGE_300}` : `${COLOR.B_50}`}
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+        </View>
+      </View>
+
+      {/* Support  */}
+      <Text style={styles.textHeading}>Support</Text>
+      <View style={styles.contentContainer}>
+        <View style={styles.itemContainer}>
+          <View style={styles.iconContainer}>
+            <SimpleLineIcons name="earphones" size={20} color={COLOR.B_300} />
+            <Text style={styles.text}>Contact Us</Text>
+          </View>
+          <Entypo
+            size={15}
+            name="chevron-thin-right"
+            color={COLOR.B_300}
+            onPress={() => navigation.navigate('Contact')}
+          />
+        </View>
+        <View style={styles.itemContainer}>
+          <View style={styles.iconContainer}>
+            <Feather
+              name="help-circle"
+              size={24}
+              color={COLOR.B_300}
+            />
+            <Text style={styles.text}>Help Center</Text>
+          </View>
+          <Entypo
+            size={15}
+            name="chevron-thin-right"
+            color={COLOR.B_300}
+            onPress={() => navigation.navigate('Contact')}
+          />
+        </View>
+        <View style={styles.itemContainer}>
+          <View style={styles.iconContainer}>
+            <MaterialIcons
+              name="feedback"
+              size={24}
+              color={COLOR.B_300}
+            />
+            <Text style={styles.text}>Give us your feedback</Text>
+          </View>
+          <Entypo
+            size={15}
+            name="chevron-thin-right"
+            color={COLOR.B_300}
+            onPress={() => navigation.navigate('Feedback')}
+          />
+        </View>
+      </View>
+
+      <Text style={styles.textHeading}>Legal</Text>
+      <View style={styles.contentContainer}>
+        <View style={styles.itemContainer}>
+          <View style={styles.iconContainer}>
+            <MaterialIcons name="privacy-tip" size={24} color={COLOR.B_300} />
+            <Text style={styles.text}>Privacy Policy</Text>
+          </View>
+          <Entypo
+            size={15}
+            name="chevron-thin-right"
+            color={COLOR.B_300}
+            onPress={() => navigation.navigate('Privacy')}
+          />
+        </View>
+      </View>
+
+
+      {/* Sign in or out  */}
+      <View style={{
+        ...styles.itemContainer,
+        marginTop: 20,
+        borderRadius: 10,
+      }}>
+        <Pressable style={styles.iconContainer}
+          onPress={handleLoginLogout}
+        >
+          <AntDesign name={isLoggedIn ? "logout" : "login"} size={25} color={COLOR.B_300} />
+          <Text
+            style={{
+            ...styles.text,
+            fontSize: FONTSIZE.TITLE_1
+          }}
+          >
+            {isLoggedIn ? 'Logout' : 'Login'}
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  )
+}
+
+export default MoreDrawerScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: COLOR.WHITE,
+    paddingTop:50
+  },
+  contentContainer: {
+    borderRadius: 10,
+    paddingVertical: 5,
+    margin: 10,
+    backgroundColor: COLOR.WHITE,
+    elevation: 1,    
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  iconContainer: {
+    gap: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textHeading: {
+    fontFamily: 'RalewayBold',
+    fontSize: FONTSIZE.TITLE_1,
+    opacity: 0.5,
+    marginLeft:10
+  },
+  text: {
+    fontFamily: 'ComfortaaBold',
+    fontSize:FONTSIZE.TITLE_2
+  }
+})
