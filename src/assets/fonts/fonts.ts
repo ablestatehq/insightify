184ba338642b8b28a294db07d1a07cdb2cc67b54
wasswaenockmale <enockmale97@gmail.com>
2023-12-08 15:55:06 +0300

    function to load a fonts

--- src/assets/fonts/fonts.ts
+++ src/assets/fonts/fonts.ts
@@ -1,30 +1,25 @@
 import { useFonts } from "expo-font";
 
-const fontNames = {
-  "ComfortaaLight": require('../fonts/Comfortaa/static/Comfortaa-Light.ttf'),
-  "Comfortaa_Regular": require('../fonts/Comfortaa/static/Comfortaa-Regular.ttf'),
-  "ComfortaaMedium": require('../fonts/Comfortaa/static/Comfortaa-Medium.ttf'),
-  "ComfortaaSemiBold": require('../fonts/Comfortaa/static/Comfortaa-SemiBold.ttf'),
-  "ComfortaaBold": require('../fonts/Comfortaa/static/Comfortaa-Bold.ttf'),
-  "RalewayThin": require('../fonts/Raleway/static/Raleway-Thin.ttf'),
-  "RalewayExtraLight": require('../fonts/Raleway/static/Raleway-ExtraLight.ttf'),
-  "RalewayLight": require('../fonts/Raleway/static/Raleway-Light.ttf'),
-  "RalewayRegular": require('../fonts/Raleway/static/Raleway-Regular.ttf'),
-  "RalewayMedium": require('../fonts/Raleway/static/Raleway-Medium.ttf'),
-  "RalewaySemiBold": require('../fonts/Raleway/static/Raleway-SemiBold.ttf'),
-  "RalewayBold": require('../fonts/Raleway/static/Raleway-Bold.ttf'),
-  "RalewayExtraBold": require('../fonts/Raleway/static/Raleway-ExtraBold.ttf'),
-  "RalewayBlack": require('../fonts/Raleway/static/Raleway-Black.ttf')
-};
-
-// async function* promiseGenerator<T>(promise: Promise<T>): AsyncGenerator<T>{
-//   yield await promise;
-// }
-
-export async function loadFonts() {
-  let [fontsLoaded, fontError] = useFonts(fontNames);
-  return [
+export function fontsLoading() {
+    let [fontsLoaded, fontError] = useFonts({
+    "ComfortaaLight": require('./Comfortaa/static/Comfortaa-Light.ttf'),
+    "Comfortaa_Regular": require('./Comfortaa/static/Comfortaa-Regular.ttf'),
+    "ComfortaaMedium": require('./Comfortaa/static/Comfortaa-Medium.ttf'),
+    "ComfortaaSemiBold": require('./Comfortaa/static/Comfortaa-SemiBold.ttf'),
+    "ComfortaaBold": require('./Comfortaa/static/Comfortaa-Bold.ttf'),
+    "RalewayThin": require('./Raleway/static/Raleway-Thin.ttf'),
+    "RalewayExtraLight": require('./Raleway/static/Raleway-ExtraLight.ttf'),
+    "RalewayLight": require('./Raleway/static/Raleway-Light.ttf'),
+    "RalewayRegular": require('./Raleway/static/Raleway-Regular.ttf'),
+    "RalewayMedium": require('./Raleway/static/Raleway-Medium.ttf'),
+    "RalewaySemiBold": require('./Raleway/static/Raleway-SemiBold.ttf'),
+    "RalewayBold": require('./Raleway/static/Raleway-Bold.ttf'),
+    "RalewayExtraBold": require('./Raleway/static/Raleway-ExtraBold.ttf'),
+    "RalewayBlack": require('./Raleway/static/Raleway-Black.ttf')
+  }
+    );
+  return {
     fontsLoaded,
     fontError
-  ]
-};
+  }
+}
