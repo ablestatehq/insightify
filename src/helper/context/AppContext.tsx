1689a8020c2e6db4e8ac75ae678f08dbb9ff60e8
wasswaenockmale <enockmale97@gmail.com>
2023-12-15 10:15:53 +0300

    sorted the opportunities to display the most recent

--- src/helper/context/AppContext.tsx
+++ src/helper/context/AppContext.tsx
@@ -3,17 +3,11 @@ import React, {
   createContext,
   useEffect,
 } from 'react';
-import DatabaseService from '../../appwrite/appwrite';
-import { environments } from '../../constants/environments';
 import codeTipsData from "../../utils/data/codeTipsData.json";
-import opportunityData from '../../../src/utils/data/opportunities.json'
 import { retrieveLocalData } from '../../utils/localStorageFunctions';
-
-const {
-  APPWRITE_DATABASE_ID,
-  APPWRITE_CODETIPS_COLLECTION_ID,
-  APPWRITE_OPPORTUNITIES_COLLECTION_ID
-} = environments;
+import opportunityData from '../../../src/utils/data/opportunities.json'
+import DatabaseService, { appwriteClient } from '../../appwrite/appwrite';
+import { APPWRITE_DATABASE_ID, APPWRITE_OPPORTUNITIES_COLLECTION_ID, APPWRITE_CODETIPS_COLLECTION_ID } from '@env';
 
 
 interface AppContextProviderProps {
@@ -25,14 +19,12 @@ interface AppContextType {
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
   codeTips: any[]
   setCodeTips: React.Dispatch<React.SetStateAction<any[]>>
-  topStories: any[]
-  setTopStories: React.Dispatch<React.SetStateAction<any[]>>
-  articles: any[]
-  setArticles: React.Dispatch<React.SetStateAction<any[]>>
   isLoggedIn: boolean
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>
   opportunities: any[]
   setOpportunities: React.Dispatch<React.SetStateAction<any[]>>
+  isNotificationEnabled: boolean
+  setIsNotificationEnabled: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export const AppContext = createContext<AppContextType>({
@@ -40,14 +32,12 @@ export const AppContext = createContext<AppContextType>({
   setIsLoading: () => { },
   codeTips: [],
   setCodeTips: () => { },
-  topStories: [],
-  setTopStories: () => { },
-  articles: [],
-  setArticles: () => { },
   isLoggedIn: false,
   setIsLoggedIn: () => { },
   opportunities: [],
-  setOpportunities: () => { }
+  setOpportunities: () => { },
+  isNotificationEnabled: false,
+  setIsNotificationEnabled: () => { }
 });
 
 const AppContextProvider = (
@@ -56,33 +46,40 @@ const AppContextProvider = (
   }: AppContextProviderProps) => {
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [codeTips, setCodeTips] = useState<any[]>(codeTipsData);
-  const [topStories, setTopStories] = useState<any[]>([])
-  const [articles, setArticles] = useState<any[]>([]);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [opportunities, setOpportunities] = useState<any[]>([]);
+  const [codeTips, setCodeTips] = useState<any[]>(codeTipsData);
+  const [isNotificationEnabled, setIsNotificationEnabled] = useState<boolean>(false);
 
 
   useEffect(() => {
-
     const fetchData = async () => {
       try {
         const bookmarked_opportunities = await retrieveLocalData('opportunities')
 
         const response =
           await DatabaseService.getDBData(
-            APPWRITE_DATABASE_ID,
             APPWRITE_OPPORTUNITIES_COLLECTION_ID
           )
         // When there is a response from the database.
         if (response) {
           const updatedOpportunity = response.map((opp: any) => {
             return ({
+              APPWRITE_DATABASE_ID,
               ...opp,
               bookmarked: bookmarked_opportunities ? bookmarked_opportunities.includes(opp.$id) : false
             })
           })
-          setOpportunities([...updatedOpportunity]);
+
+          // Sort the opportunites to bring the lasted notifications first.
+          const sortedOpportunities = updatedOpportunity.sort((a: any, b: any) => {
+            const dateA = new Date(a?.$createdAt)
+            const dateB = new Date(b?.$createdAt)
+
+            return (dateB as any) - (dateA as any)
+          });
+
+          setOpportunities([...sortedOpportunities]);
         } else {
           // If there is no response from the database, use the opportunities stored.
           const updatedOpportunity = opportunityData.opportunities.map((opp: any) => {
@@ -96,7 +93,6 @@ const AppContextProvider = (
 
         const developerTips =
           await DatabaseService.getDBData(
-            APPWRITE_DATABASE_ID,
             APPWRITE_CODETIPS_COLLECTION_ID
           )
             .then(response => response)
@@ -115,6 +111,31 @@ const AppContextProvider = (
     }
 
     fetchData();
+
+    // const unsubscribe = appwriteClient?.subscribe(
+    //   `databases.${APPWRITE_DATABASE_ID}.collections.${APPWRITE_OPPORTUNITIES_COLLECTION_ID}.documents`,
+    //   (response: any) => {
+    //     if (response.events.includes(
+    //       'databases.*.collections.*.documents.*.create'
+    //     )) {
+    //       setOpportunities((prevOpportunities: any[]) => {
+    //         const updatedOpportunity = [...prevOpportunities, ...response.payload];
+    //         return updatedOpportunity;
+    //       })
+    //     } else if (response.events.includes(
+    //       'database.*.collections.*.documents.delete'
+    //     )) {
+    //       setOpportunities((prevOpportunities: any[]) => {
+    //         return prevOpportunities.filter((opportunity) => {
+    //           return opportunity.$id !== response.payload.$id
+    //         })
+    //       })
+    //     }
+    //   })
+
+    return () => {
+      // unsubscribe ? unsubscribe() : null
+    }
   }, []);
 
 
@@ -123,14 +144,12 @@ const AppContextProvider = (
     setIsLoading,
     codeTips,
     setCodeTips,
-    topStories,
-    setTopStories,
-    articles,
-    setArticles,
     isLoggedIn,
     setIsLoggedIn,
     opportunities,
-    setOpportunities
+    setOpportunities,
+    isNotificationEnabled,
+    setIsNotificationEnabled
   };
 
   return (
