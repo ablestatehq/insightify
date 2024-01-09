6db1939e2f1174177229dcb514659dbff9b792c2
wasswaenockmale <enockmale97@gmail.com>
2024-01-09 11:59:54 +0300

    refactor: Refactor code in Sky Screen

--- src/screens/CodeTips/CodeTips.tsx
+++ src/screens/CodeTips/CodeTips.tsx
@@ -1,35 +1,95 @@
 import {
-  ScrollView,
   StyleSheet,
+  Text,
   View,
 } from 'react-native'
 import React, { useContext } from 'react';
 // constants 
-import { COLOR } from '../../constants/contants';
+import { COLOR, DIMEN, FONTSIZE } from '../../constants/contants';
+import Carousel from 'react-native-reanimated-carousel';
 
 // components 
 import { ExpandableItemList } from '../../components';
-// import Carousel from 'react-native-snap-carousel';
-
 
 import { AppContext } from '../../helper/context/AppContext';
 import { extractCodeSnippet } from '../../helper/functions/functions';
+import CodeHighlighter from 'react-native-code-highlighter';
+import { atomOneDarkReasonable } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import CategorySection from './componets/CategorySection/CategorySection';
 
 const CodeTips = () => {
+  const { SCREENWIDTH: width, SCREENHEIGHT: height } = DIMEN;
 
   const { codeTips } = useContext(AppContext);
-  const [expandedIndex, setExpandedIndex] = React.useState<number | null>(0)
 
 
-  const handleToggleExpand = (index: number) => {
-    setExpandedIndex((prevIndex) => (prevIndex === index ? null : index))
-  }
-  
   return (
     <View
       style={styles.codeTipsContainer}
     >
-      <View
+      <View>
+        {/* <Text>Code Tips</Text> */}
+        {/* <CategorySection /> */}
+      </View>
+      <Carousel
+        loop
+        width={width}
+        height={height}
+        data={codeTips}
+        mode='parallax'
+        panGestureHandlerProps={{
+          activeOffsetX: [-10, 10]
+        }}
+        // onSnapToItem={(index) => console.log('current index: ', index)}
+        renderItem={({ item, index }) => (
+          <View style={styles.renderItemView}>
+            <View style={styles.tipTitleView}>
+              <View style={styles.categoryView}>
+                <Text style={styles.titleName}>
+                  {item?.Category}
+                </Text>
+              </View>
+              <View />
+            </View>
+            <View
+              style={
+                styles.tipContentView
+              }
+            >
+              <Text
+                numberOfLines={3}
+                style={{
+                  ...styles.tipTitle,
+                  fontFamily: "RalewayBold"
+                }}
+              >
+                # {item?.Title}
+              </Text>
+              <Text
+                style={{
+                  ...styles.tipContent,
+                }}
+              >
+                {item?.details?.replace(/```([^`]*)```/g, '')}
+              </Text>
+              <View style={styles.tipFooter}>
+                <View style={styles.codeDescriptionStyle}>
+                  <CodeHighlighter
+                    hljsStyle={atomOneDarkReasonable}
+                    containerStyle={styles.codeSnippet}
+                    textStyle={styles.code}
+                    language={`${item?.Category?.toLowerCase()}`}
+                  >
+                    {extractCodeSnippet(item?.details)}
+                  </CodeHighlighter>
+                </View>
+              </View>
+            </View >
+          </View>
+        )}
+      />
+
+      {/* <View
         style={styles.contentContainer}
       >
         <ScrollView
@@ -56,16 +116,7 @@ const CodeTips = () => {
             })
           }
         </ScrollView>
-        {/* <Carousel
-          layout={'tinder'}
-          ref={setCarouselRef}
-          data={codeTips}
-          renderItem={renderCodeTips}
-          sliderWidth={windowWidth}
-          itemWidth={windowWidth}
-          loop={true}
-        /> */}
-      </View>
+      </View> */}
     </View>
   );
 }
@@ -75,8 +126,6 @@ export default CodeTips
 const styles = StyleSheet.create({
   codeTipsContainer: {
     flex: 1,
-    padding: 10,
-    paddingTop: 10,
     backgroundColor: COLOR.WHITE,
   },
   contentContainer: {
@@ -88,5 +137,52 @@ const styles = StyleSheet.create({
   },
   scrollableCodeTips: {
 
+  },
+  renderItemView: {
+    flex: 1,
+    borderWidth: 0.2,
+    padding: 10,
+    borderRadius: 10
+  },
+  tipTitle: {
+    color: COLOR.B_300,
+    fontSize: FONTSIZE.HEADING_4,
+    // textAlign: 'center',
+    fontFamily:'RalewayBold'
+  },
+  tipContent: {
+    color: COLOR.B_300,
+    fontSize: FONTSIZE.HEADING_5,
+    fontFamily: 'ComfortaaBold',
+    lineHeight:30
+  },
+  tipContentView: {
+    flex:1,
+    // borderWidth: 1,
+    padding:10
+  },
+  tipFooter: {
+    
+  },
+  tipTitleView: {
+    marginBottom: 10,
+    justifyContent:'flex-start'
+  },
+  titleName: {
+    textTransform: 'capitalize',
+    fontSize:FONTSIZE.TITLE_1
+  },
+  categoryView: {
+    padding:10
+  },
+  codeSnippet: {
+    paddingHorizontal:20
+  },
+  codeDescriptionStyle: {
+    
+  },
+  code: {
+    fontFamily: 'RalewayBold',
+    fontSize:FONTSIZE.BODY
   }
 });
