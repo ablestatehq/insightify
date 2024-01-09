aca9be34689c0aa307d5e3dce2a04d7c51953cf6
wasswaenockmale <enockmale97@gmail.com>
2024-01-09 11:55:10 +0300

    chore: Add new environmental variable and update env.d.ts file

--- env.d.ts
+++ env.d.ts
@@ -3,11 +3,12 @@ declare module '@env' {
   export const STRAPI_TOKEN: string
   export const TECH_IN_AFRICA:string
   export const AUTHOR_ENDPOINT:string
-  export const STRAPI_BASE_URL:string
+  export const STRAPI_BASE_URL: string
   export const APPWRITE_API_KEY:string
   export const APPWRITE_PROJECT_ID:string
   export const APPWRITE_DATABASE_ID: string
   export const APPWRITE_PROJECT_ENDPOINT:string
+  export const STRAPI_TALENT_FORM_API_KEY:string
   export const APPWRITE_USER_COLLECTION_ID:string
   export const APPWRITE_CODETIPS_COLLECTION_ID:string
   export const APPWRITE_ARTICLES_COLLECTION_ID:string
