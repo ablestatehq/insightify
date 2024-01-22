00324bee5b84b1b5b9ae6a98f321574f94d0fc3d
wasswaenockmale <enockmale97@gmail.com>
2024-01-22 16:36:36 +0300

    fix: post request bag

--- api/strapiJSAPI.ts
+++ api/strapiJSAPI.ts
@@ -1,11 +1,12 @@
 import { STRAPI_TOKEN, STRAPI_BASE_URL,STRAPI_TALENT_FORM_API_KEY } from "@env"
 
-// API endpoint /suggestions
-// API fields
-// - rating
-// - ⁠improvements
-// - ⁠suggestion
-// Function to save opportunities to strapiJS database.
+/**
+ * @name storeData
+ * @param endpoint 
+ * @param data 
+ * @returns 
+ * Function to save opportunities to strapiJS database
+ */
 async function storeData(endpoint: string, data: any) {
   // posting data to strapi-backend server
   const payload = {
@@ -27,26 +28,112 @@ async function storeData(endpoint: string, data: any) {
   return response
 }
 
-// retrieve data from strapi
+/**
+ * @name getStrapiData
+ * @param endpoint 
+ * @returns 
+ * retrieve data from strapi
+ */
 async function getStrapiData(endpoint: string) {
   const options = {
     method: 'GET',
     headers: {
     'content-type': 'application/json',
-    'Authorization': `Bearer ${STRAPI_TOKEN}`
+      'Authorization': `Bearer ${endpoint == 'notification-tokens' ? STRAPI_TALENT_FORM_API_KEY : STRAPI_TOKEN}`
     }
   }
+
+  // console.log(`${endpoint}`,options)
   try {
     const response = await fetch(`${STRAPI_BASE_URL}${endpoint}`, options)
       .then(response => response.json())
-      .then(data => data.data.map((res: any) => res.attributes))
+      .then(data => {
+        if (endpoint == 'notification-tokens') {
+          return data.data.map((res: any) => res.attributes.tokenID)
+        }
+        return data.data.map((res: any) => res.attributes)
+      })
       .catch((error:any) => { console.log(error.message) });
     return response;
   } catch (error) {
   }
 }
 
+
+/**
+ * Update document in strapiJS
+ * @name updateStrapiData()
+ * @param endpoint
+ * @param id
+ * @param data
+ * @returns 
+ */
+async function updateStrapiData(endpoint: string, id: number, data: any) {
+  const payload = {
+    'data': data
+  };
+
+  const options = {
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': `Bearer ${STRAPI_TALENT_FORM_API_KEY}`
+    },
+    body: JSON.stringify(payload)
+  }
+
+  try {
+    const response = await
+      fetch(`${STRAPI_BASE_URL}${endpoint}`, options)
+        .then(response => response.json)
+        .then(data => data)
+        .catch(error => console.warn(error))
+    
+    return response
+
+  } catch (error) {
+    return null
+  }
+}
+
+/**
+ * @name getDataId
+ * @param endpoint
+ * @param attribute
+ * @param attributeValue
+ * @return id
+ */
+async function getDataId(endpoint: string, attribute: string, attributeValue: any) {
+  const options = {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+      'Authorization':`Bearer ${STRAPI_TALENT_FORM_API_KEY}`
+    }
+  }
+
+  try {
+    const response = await
+      fetch(`${STRAPI_BASE_URL}${endpoint}`, options)
+        .then(response => response.json())
+        .then(data => data.data.find((res: any) => {
+          if (res.attributes[attribute] == attributeValue) {
+            return res.attributes.id
+          }
+        }))
+        .catch(error => console.warn(error))
+    
+    return response
+  } catch (error) {
+    // return {
+    //   status: 'error',
+    //   message: error
+    // }
+  }
+}
 export {
   storeData,
-  getStrapiData
+  getDataId,
+  getStrapiData,
+  updateStrapiData
 }
