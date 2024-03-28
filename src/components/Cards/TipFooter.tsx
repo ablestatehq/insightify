61cd9d8540e6c5fddd5a7dbc31fa337a79ad9e97
wasswaenockmale <enockmale97@gmail.com>
2024-03-28 12:44:57 +0300

    Style: Reduce the size of the items

--- src/components/Cards/TipFooter.tsx
+++ src/components/Cards/TipFooter.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import {Ionicons} from '@expo/vector-icons';
 import {Pressable, StyleSheet, Text, View} from 'react-native';
 
-import Icons from '../../assets/icons';
+import Icon from '../../assets/icons';
 import OpenLink from '../../utils/OpenLink';
-import {COLOR} from '../../constants/contants';
+import IconLabelPair from '../IconLabelPair';
+import {COLOR, FONTSIZE} from '../../constants/contants';
+import {CommentSection} from '..';
 
 interface TipFooterProps {
+  id?: number
   source_url_text?: string
   source_url?: string
   isCodeTip?: boolean
@@ -14,69 +16,88 @@ interface TipFooterProps {
   handleBookmark: () => void
   onSubmitReport: () => void
 }
+
 const TipFooter: React.FC<TipFooterProps> = ({
+  id,
   source_url_text,
   isCodeTip = false,
   bookmarked,
   handleBookmark,
   source_url,
-  onSubmitReport
+  onSubmitReport,
 }) => {
-  {/* footer  */ }
+  // State
+  const [openComments, setOpenComments] = React.useState<boolean>(false);
+
+  // Footer functions
   const openSourceLink = () => {
     OpenLink(source_url as string)
-  }
+  };
+
   const handleSubmit = () => {
     onSubmitReport()
   }
+
+  // const fetchComments = React.useMemo(() => {
+  //   const response = dataModal.getData('comments');
+  //   return response;
+  // }, [id]);
+
+  React.useEffect(() => {
+    
+   }, []);
   return (
-    <View style={styles.footer}>
-      {!bookmarked && <Ionicons
+    <View style={[styles.footer]}>
+      {!bookmarked && <Icon
         name="heart-outline"
-        size={30}
-        color={COLOR.PRIMARY_300}
-        onPress={handleBookmark} />}
-      {bookmarked && <Ionicons
+        size={20}
+        _color={COLOR.PRIMARY_300}
+        press={handleBookmark} />}
+      {bookmarked && <Icon
         name='heart'
-        size={25}
-        color={COLOR.PRIMARY_300}
-        onPress={handleBookmark}
+        size={20}
+        _color={COLOR.PRIMARY_300}
+        press={handleBookmark}
       />}
-      {/* A user should be able to report anthing wrong with the tip/opportunity.*/}
-      <Icons name='report' press={handleSubmit} size={25}/>
+
+      {/* {!bookmarked && <IconLabelPair iconName='heart-outline' iconLabel='Save' onPress={handleBookmark} />} */}
+      {/* {bookmarked && <IconLabelPair iconName='heart' iconLabel='Saved' onPress={handleBookmark} />} */}
+
+      {/* A user should be able to report anthing wrong with the tip/opportunity. */}
+      {/* <IconLabelPair iconName='report' iconLabel={'Report'} size={20} onPress={handleSubmit} /> */}
+      {/* <IconLabelPair iconLabel='Comment' iconName='message' size={17} onPress={function () { setOpenComments(true) }} /> */}
+      <Icon name='report' press={handleSubmit} size={20} />
       {source_url &&
-        <View>
-          <Pressable style={styles.inputView} onPress={openSourceLink}>
-            <Text style={styles.text}>{source_url_text}</Text>
-          </Pressable>
-        </View>}
+        <Pressable style={styles.inputView} onPress={openSourceLink}>
+          <Text style={styles.text}>{source_url_text}</Text>
+        </Pressable>
+      }
     </View>
   );
 }
 
-export default React.memo(TipFooter)
+export default React.memo(TipFooter);
 
 const styles = StyleSheet.create({
   footer: {
     flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    gap: 10,
-    marginTop: 20
+    gap: 5,
+    marginTop: 20,
+    justifyContent:'flex-end'
   },
   inputView: {
     borderWidth: 1,
     borderColor: COLOR.PRIMARY_300,
-    borderRadius: 10,
+    borderRadius: 5,
     paddingVertical: 5,
-    paddingHorizontal: 20,
+    paddingHorizontal: 5,
     backgroundColor: COLOR.PRIMARY_50
   },
   inputText: {
     flex: 1,
-    // padding:5 
   },
   text: {
-    color: COLOR.PRIMARY_300
+    color: COLOR.PRIMARY_300,
+    fontSize:FONTSIZE.SMALL
   }
 })
