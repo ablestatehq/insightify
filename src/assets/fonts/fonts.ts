37a31ed80f597b86687aeb66c7472ee070491b3f
wasswaenockmale <enockmale97@gmail.com>
2024-07-25 16:27:02 +0300

    chore: Update font files and remove old ones

--- src/assets/fonts/fonts.ts
+++ src/assets/fonts/fonts.ts
@@ -1,28 +1,11 @@
-import { useFonts } from "expo-font";
+export const FONT_NAMES = {
+  Heading: 'BoldHeading',
+  Title: 'BoldTitle',
+  Body: 'BodyText',
+};
 
-export function fontsLoading() {
-  let [fontsLoaded, fontError] = useFonts({
-    "ComfortaaLight": require('./Comfortaa/static/Comfortaa-Light.ttf'),
-    "Comfortaa_Regular": require('./Comfortaa/static/Comfortaa-Regular.ttf'),
-    "ComfortaaMedium": require('./Comfortaa/static/Comfortaa-Medium.ttf'),
-    "ComfortaaSemiBold": require('./Comfortaa/static/Comfortaa-SemiBold.ttf'),
-    "ComfortaaBold": require('./Comfortaa/static/Comfortaa-Bold.ttf'),
-    "RalewayThin": require('./Raleway/static/Raleway-Thin.ttf'),
-    "RalewayExtraLight": require('./Raleway/static/Raleway-ExtraLight.ttf'),
-    "RalewayLight": require('./Raleway/static/Raleway-Light.ttf'),
-    "RalewayRegular": require('./Raleway/static/Raleway-Regular.ttf'),
-    "RalewayMedium": require('./Raleway/static/Raleway-Medium.ttf'),
-    "RalewaySemiBold": require('./Raleway/static/Raleway-SemiBold.ttf'),
-    "RalewayBold": require('./Raleway/static/Raleway-Bold.ttf'),
-    "RalewayExtraBold": require('./Raleway/static/Raleway-ExtraBold.ttf'),
-    "RalewayBlack": require('./Raleway/static/Raleway-Black.ttf'),
-    "LatoLight": require('./lato/Lato-Light.ttf'),
-    "LatoRegular": require('./lato/Lato-Regular.ttf'),
-    "LatoBold": require('./lato/Lato-Bold.ttf')
-  });
-  
-  return {
-    fontsLoaded,
-    fontError
-  }
-}
+export const FONT_FILES = {
+  [FONT_NAMES.Heading]: require('./fonts/Poppins-SemiBold.ttf'),
+  [FONT_NAMES.Title]: require('./fonts/Poppins-Medium.ttf'),
+  [FONT_NAMES.Body]: require('./fonts/LibreFranklin-Regular.ttf'),
+};
