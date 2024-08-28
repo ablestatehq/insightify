dc04dd7a4e07c9f6c2e06719c01bb75109874b42
wasswaenockmale <enockmale97@gmail.com>
2024-08-28 18:29:28 +0300

    fix: user can only get points for views not more than 3 times

--- src/utils/awardXP.ts
+++ src/utils/awardXP.ts
@@ -1,11 +1,20 @@
+import {BASE_URL} from "@env";
 import {storeData} from "../../api/strapiJSAPI";
 import {retrieveLocalData, storeToLocalStorage} from "./localStorageFunctions";
 
   // Give points to the user for viewing the product.
 const awardXP = async (AWARD: any, itemID: number, authToken: string, userID: number) => {
   const award = await retrieveLocalData('award-token');
-  
-  if (award && award[itemID]) {
+  const views = await
+    (await fetch(`${BASE_URL}/api/views?filters[resourceId][$eq]=${itemID}&populate[user][filters][id][$eq]=${userID}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${authToken}`
+      }})).json();
+  // console.log(views.data.length);
+  if (award && award[itemID] && views.data.length >= 3) {
     if (award[itemID] !== 'THIRD') {
       await storeData(
         'rewards',
@@ -29,7 +38,10 @@ const awardXP = async (AWARD: any, itemID: number, authToken: string, userID: nu
       return AWARD[award[itemID] === 'FIRST' ? 'SECOND' : 'THIRD'] ?? 0
     }
   } else {
-    await storeData(
+    if (views.data.length >= 0) {
+      return
+    } else {
+      await storeData(
       'rewards',
       {
         event: 1,
@@ -40,7 +52,7 @@ const awardXP = async (AWARD: any, itemID: number, authToken: string, userID: nu
         user: userID,
       }, authToken);
     
-    const viewResponse = await storeData(
+    await storeData(
         'views',
         {
           user: userID,
@@ -52,6 +64,7 @@ const awardXP = async (AWARD: any, itemID: number, authToken: string, userID: nu
     
     await storeToLocalStorage('award-token', {...award, [itemID]: 'FIRST'});
     return AWARD['FIRST'] ?? 0;
+    }
   }
 }
 
