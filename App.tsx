a73d126df6bb923bcd80d931f3dd7b2fead1ad8c
wasswaenockmale <enockmale97@gmail.com>
2024-08-30 11:57:02 +0300

    fix: Add the types for the navigationRef

--- App.tsx
+++ App.tsx
@@ -1,34 +1,60 @@
 import {useEffect} from 'react';
 import {Linking} from 'react-native';
 import * as Updates from 'expo-updates';
+import * as TaskManager from 'expo-task-manager';
+
 import {MainStackNavigator} from './src/routes/StackNavigator';
 import AppContextProvider from './src/helper/context/AppContext';
 import usePushNotifications from './src/helper/customHooks/usePushNotification';
-import {NavigationContainer, useNavigationContainerRef} from '@react-navigation/native';
+import {NavigationContainer, ParamListBase, useNavigationContainerRef} from '@react-navigation/native';
+import {BGTASKS} from './src/constants/constants';
+import {
+  useNetworkStatus,
+  registerBackgroundFetchAsync,
+  unregisterBackgroundFetchAsync,
+} from './src/helper/customHooks/useNetworkStatus';
+
+import {useFonts} from 'expo-font';
+import {FONT_FILES} from './src/assets/fonts/fonts';
 
 export default function App() {
 
-  const {currentlyRunning, isUpdateAvailable, isUpdatePending} = Updates.useUpdates();
-  const navigationRef = useNavigationContainerRef();
+  const [fontsLoaded] = useFonts(FONT_FILES);
+  const {isUpdatePending} = Updates.useUpdates();
+  const navigationRef = useNavigationContainerRef<ParamListBase>();
+
   useEffect(() => {
     const handleDeepLink = async (event: {url: string}) => {
       const {url} = event;
-      let data = await Linking.canOpenURL(url)
+      let data = await Linking.canOpenURL(url);
       if (data) {
         navigationRef.current?.navigate("Home");
       }
     };
 
     // Add event listener for deep links
-    Linking.addEventListener('url', handleDeepLink)
+    Linking.addEventListener('url', handleDeepLink);
 
     if (isUpdatePending) {
       Updates.reloadAsync();
     }
-  }, [isUpdatePending])
-  const {expoPushToken} = usePushNotifications()
+
+  }, [isUpdatePending]);
+
+  // Register the push notification for the app.
+  usePushNotifications();
+
+
+  TaskManager.defineTask(BGTASKS.CHECK_ONLINE_STATUS, useNetworkStatus);
+  useEffect(() => {
+    registerBackgroundFetchAsync();
+    return () => {
+      unregisterBackgroundFetchAsync();
+    };
+  }, []);
+
   return (
-    <NavigationContainer ref={navigationRef}>
+    <NavigationContainer ref={navigationRef} >
       <AppContextProvider>
         <MainStackNavigator />
       </AppContextProvider>
