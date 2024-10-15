ac34d84f05320198f25eedfcee31e5c46bbc2690
wasswaenockmale <enockmale97@gmail.com>
2024-10-15 11:52:41 +0300

    refactor: improve logic to improve chat screen performance

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -1,34 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {StyleSheet, Text, View} from 'react-native'
 import React from 'react'
-import { Ionicons } from '@expo/vector-icons';
-import { COLOR, FONTSIZE } from '../../constants/constants';
-import { useNavigation } from '@react-navigation/native';
-import { FONT_NAMES } from '../../assets/fonts/fonts';
+import {Ionicons, Octicons, AntDesign} from '@expo/vector-icons';
+import {COLOR, FONTSIZE } from '../../constants/constants';
+import {useNavigation} from '@react-navigation/native';
+import {FONT_NAMES} from '../../assets/fonts/fonts';
 
 interface HeaderProps {
   title?: string
+  iconColor?: string,
+  backgroundColor?: string;
+  showMore?: boolean;
+  // mgs: any;
+  selected: string
+  onClose: () => void;
+  onReply: (msgMapID: string) => void;
+  onDelete: (key: string) => void;
 }
 const Header = (
   {
-    title = ""
+    showMore=false,
+    title = "",
+    iconColor = "black",
+    backgroundColor,
+    onDelete,
+    onReply,
+    onClose,
+    selected,
   }: HeaderProps
 ) => {
 
   const navigation = useNavigation();
-
+  const handleBack = () => {navigation.goBack()};
+  const handleDelete = () => {
+    if (selected.trim()) {
+      onDelete(selected.trim());
+      onClose();
+    }
+  };
+  const handleReply = () => {
+    if (selected.trim()) {
+      onReply(selected.trim());
+      onClose()
+    }
+  };
   return (
     <View
-      style={styles.container}
+      style={[styles.container, {backgroundColor}]}
     >
       <Ionicons
         name="arrow-back"
         size={20}
-        color="black"
-        onPress={() => {
-          navigation.goBack()
-        }}
+        color={iconColor}
+        onPress={handleBack}
       />
-      <Text
+      {!showMore ? (<Text
         style={[
           styles.text,
           {
@@ -37,8 +62,15 @@ const Header = (
         ]}
       >
         {title}
-      </Text>
-      <View />
+      </Text>) :
+        (<View style={{flexDirection: 'row', gap: 10}}>
+          <Octicons name="reply" size={15} color="black" onPress={handleReply} style={styles.icon} />
+          <AntDesign name="delete" size={15} color="black" onPress={handleDelete} style={styles.icon} />
+          {/* <AntDesign name="staro" size={15} color="black" /> */}
+          {/* <AntDesign name="star" size={15} color="black" /> */}
+          {/* <Feather name="more-vertical" size={15} color="black" /> */}
+      </View>)}
+      {!showMore && <View />}
     </View >
   )
 }
@@ -51,11 +83,16 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 10,
+    backgroundColor: COLOR.NEUTRAL_2,
+    paddingBottom: 5,
     // borderWidth: 1,
     // borderColor: COLOR.DANGER
   },
   text: {
     fontSize: FONTSIZE.TITLE_2,
     // paddingBottom: 2.5
+  },
+  icon: {
+    padding: 2.5,
   }
 })
--- src/screens/ChatScreen/ChatScreen.tsx
+++ src/screens/ChatScreen/ChatScreen.tsx
@@ -5,8 +5,6 @@ import Header from '../../components/Headers/Header';
 import Message from '../../components/Cards/Message';
 import useChat from '../../helper/customHooks/useChat';
 import {AppContext} from '../../helper/context/AppContext';
-import Octicons from '@expo/vector-icons/Octicons';
-import AntDesign from '@expo/vector-icons/AntDesign';
 
 import {
   View, FlatList, StyleSheet,
@@ -17,12 +15,10 @@ import {SendCard} from '../../components';
 const ChatScreen = () => {
   const {user, jwt} = useContext(AppContext);
   const {
-    // refs
-    textInputRef,
-    // states
+    selected,
+    setSelected,
     messageMap,
     messages,
-    onCloseReply,
     replyingTo,
     refreshing,
     newMessage,
@@ -36,48 +32,56 @@ const ChatScreen = () => {
     handleEndReached,
     onReaction,
     onReply,
+    onCloseReply,
   } = useChat(user.id, jwt);
 
-  const renderMessage = React.useCallback(({item, index}: {item: string, index:number}) => {
+  const sendMessage = () => handleSendMessage(replyingTo as string);
+  const renderMessage = React.useCallback(({item}: {item: string}) => {
     const msg = messageMap.get(item);
     return (
-      <Message
-        userId={user.id}
-        onReaction={onReaction}
-        onReply={onReply}
-        onDelete={handleDeleteMessage}
-        {...msg}
-      />
+      <View
+        key={item}
+        style={{
+          margin: DIMEN.PADDING.ES,
+          padding: DIMEN.PADDING.ES,
+        }}
+      >
+        <Message
+          key={item}
+          userId={user.id}
+          msgKey={item}
+          selected={selected}
+          onReaction={onReaction}
+          setSelectedMsg={setSelected}
+          handleModalVisibility={handleModalVisibility}
+          modalVisibility={modalVisibility}
+          {...msg}
+        />
+     </View>
     );
-  }, [messageMap, user.id, onReaction, onReply, handleDeleteMessage]);
+  }, [messageMap]);
   
   return (
     <View style={styles.container}>
-      <View>
-        <View style={styles.contentHeader}>
-          <View style={{flex: 1}}>
-            <Header title='Chat' backgroundColor={COLOR.WHITE} />
-          </View>
-          {modalVisibility &&
-            <View>
-              <Pressable onPress={() => {
-                handleModalVisibility();
-              }}>
-                <Octicons name="reply" size={15} color="black" />
-              </Pressable>
-              <Pressable>
-                <AntDesign name="delete" size={15} color="black" />
-              </Pressable>
-            </View>
-          }
+      <View style={styles.contentHeader}>
+        <View style={styles.navStyle}>
+          <Header
+            title='Chat'
+            backgroundColor={COLOR.WHITE}
+            showMore={modalVisibility}
+            selected={selected}
+            onReply={onReply}
+            onDelete={handleDeleteMessage}
+            onClose={handleModalVisibility}
+          />
         </View>
       </View>
-      <View style={{flex: 1, padding: 15}}>
-        {refreshing && (
-          <View style={{alignItems: 'center', padding: 5, backgroundColor:'Transparent'}}>
+      <View style={styles.messagesContainer}>
+        {refreshing ? (
+          <View style={styles.refreshStyle}>
             <ActivityIndicator size="small" color={COLOR.PRIMARY_200} />
           </View>
-        )}
+        ) : null}
         <FlatList
           data={messages}
           inverted
@@ -94,24 +98,14 @@ const ChatScreen = () => {
           messageMap={messageMap}
           closeReply={onCloseReply}
           userId={user.id}
-          textInputRef={textInputRef}
           newMessage={newMessage}
           setNewMessage={setNewMessage}
         />
         <Pressable
-          style={{
-            backgroundColor: COLOR.PRIMARY_300,
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: 40,
-            height: 40,
-            borderRadius: 30,
-            paddingLeft: 5,
-            padding: 2.5,
-          }}
-          onPress={() => handleSendMessage(replyingTo as string)}
+          style={styles.sendBtnStyle}
+          onPress={sendMessage}
         >
-          <Ionicons name="send" size={18} color="white" />
+          <Ionicons name="send" size={18} color={COLOR.WHITE} />
         </Pressable>
       </View>
     </View>
@@ -132,7 +126,12 @@ const styles = StyleSheet.create({
     paddingRight: DIMEN.PADDING.ME,
     justifyContent: 'space-between',
   },
+  messagesContainer: {
+    flex: 1, padding: 15,
+    backgroundColor: 'transparent',
+  },
   inputContainer: {
+    backgroundColor: 'transparent',
     flexDirection: 'row',
     alignItems: 'flex-end',
     borderRadius: 5,
@@ -142,10 +141,24 @@ const styles = StyleSheet.create({
   messageContainer: {
     flexDirection: 'row',
     marginBottom: 10,
-    padding: 10,
+    // padding: 10,
     borderRadius: 10,
   },
   content: {},
-
-  
+  sendBtnStyle: {
+    backgroundColor: COLOR.PRIMARY_300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 40,
+    height: 40,
+    borderRadius: 30,
+    paddingLeft: 5,
+    padding: 2.5,
+  },
+  refreshStyle: {
+    alignItems: 'center',
+    padding: 5,
+    backgroundColor: 'Transparent',
+  },
+  navStyle: { flex: 1 }
 });
