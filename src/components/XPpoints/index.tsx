27228b30734ce677c4a317a5e836e104ad096e3e
wasswaenockmale <enockmale97@gmail.com>
2024-10-29 12:59:40 +0300

    style: refine XPpoint UI

--- src/components/XPpoints/index.tsx
+++ src/components/XPpoints/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {StyleSheet, Text, View} from 'react-native';
 import {FontAwesome6} from '@expo/vector-icons';
-import {COLOR, DIMEN} from '../../constants/constants';
+import {COLOR, DIMEN, FONTSIZE} from '../../constants/constants';
 import {FONT_NAMES} from '../../assets/fonts/fonts';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
@@ -17,13 +17,13 @@ const Index = ({number, navigation, inCommunity}: XPpointProps) => {
   return (
     <View style={styles.container}>
       <View style={styles.xpView}>
-        <FontAwesome6 name="bolt" size={12} color={COLOR.GOLD} />
+        <FontAwesome6 name="bolt" size={12} color={COLOR.GREY_300} />
         <Text style={styles.points}>{number} XP</Text>
       </View>
       {inCommunity && <Ionicons
         name="chatbubbles-outline"
         size={15}
-        color={COLOR.GOLD}
+        color={COLOR.GREY_300}
         style={styles.icon}
         onPress={() => navigation.navigate('ChatRoom')} />}
     </View>
@@ -34,22 +34,29 @@ export default React.memo(Index);
 
 const styles = StyleSheet.create({
   container: {
-    gap: 10,
-    borderRadius: 5,
+    gap: DIMEN.CONSTANT.SM,
+    borderRadius: DIMEN.CONSTANT.SM,
     alignItems: 'center',
     flexDirection: 'row',
-    paddingHorizontal: 10,
     alignSelf: 'flex-end',
-    padding: 5,
   },
   xpView: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 3.5
+    gap: 3.5,
+    height: 25,
+    width: 64,
   },
   points: {
     fontFamily: FONT_NAMES.Title,
-    color: COLOR.SILVER,
+    color: COLOR.GREY_200,
+    flex: 1,
+    // borderWidth: 1,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    lineHeight: 18,
+    padding: DIMEN.PADDING.SM,
+    fontSize: FONTSIZE.BODY
   },
   icon: {
     paddingLeft: DIMEN.PADDING.ME
