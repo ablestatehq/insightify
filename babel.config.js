99bb7c9da5a831be7cd1bd6733d9fc3f46ce8125
wasswaenockmale <enockmale97@gmail.com>
2024-11-01 13:16:49 +0300

    chore: add plugin for absolute imports

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,32 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-    ["module:react-native-dotenv", {
-      "envName": "APP_ENV",
-      "moduleName": "@env",
-      "path": ".env",
-      }],
+      [
+        "module:react-native-dotenv",
+        {
+          "envName": "APP_ENV",
+          "moduleName": "@env",
+          "path": ".env",
+        }],
       "react-native-reanimated/plugin",
-  ]
+      ["module-resolver",
+      {
+        "alias": {
+          "@src": "./src",
+          "@assets": "./assets",
+          "@api": "./api",
+          "@assets_": "./src/assets",
+          "@components": "./src/components",
+          "@constants": "./src/constants",
+          "@helpers": "./src/helper",
+          "@lib": "./src/lib",
+          "@routes": "./src/routes",
+          "@screens": "./src/routes",
+          "@utils": "./src/utils",
+          "@fonts": "./src/assets/fonts/fonts.ts",
+          "@icons": "./src/assets/icons/index.tsx"
+        },
+      },]
+    ]
   };
 };
