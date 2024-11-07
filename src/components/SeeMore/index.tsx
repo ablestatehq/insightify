93196019a3505ae92485a1c78799999318b6bd24
wasswaenockmale <enockmale97@gmail.com>
2024-11-07 11:16:42 +0300

    style(seemore): add more descriptive tests

--- src/components/SeeMore/index.tsx
+++ src/components/SeeMore/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { FONTSIZE } from '@constants/constants';
+import { DIMEN, FONTSIZE } from '@constants/constants';
 import { FONT_NAMES } from '@fonts';
 
 interface SeeMoreSectionProps {
   title: string;
+  text?: string;
   onPress?: () => void;
 }
-const Index = ({ title, onPress }: SeeMoreSectionProps) => {
+const Index = ({ title, onPress, text}: SeeMoreSectionProps) => {
   return (
     <View style={styles.seeMoreContainer}>
       <Text style={styles.eventText}>{title}</Text>
       {onPress && (
         <TouchableOpacity onPress={onPress}>
-          <Text style={styles.seeMoreText}>see more</Text>
+          <Text style={styles.seeMoreText}>See more {text}</Text>
         </TouchableOpacity>
       )}
     </View>
@@ -26,15 +27,13 @@ const styles = StyleSheet.create({
   seeMoreText: {
     fontFamily: FONT_NAMES.Body,
     fontSize: FONTSIZE.SMALL,
-    textTransform: 'uppercase',
   },
   seeMoreContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    // marginVertical: 5,
-    // borderWidth: 1,
-    // borderColor: COLOR.DANGER,
+    margin: DIMEN.MARGIN.XXSM,
+
   },
   eventText: {
     fontSize: FONTSIZE.BODY,
--- src/components/index.ts
+++ src/components/index.ts
@@ -33,3 +33,4 @@ export {default as MessageList} from './MessageList'
 export {default as EmptyState} from './EmptyState'
 export {default as Dialog} from './Dialog';
 export {default as HomeItem} from './HomeItemCard';
+export {default as Message} from './Cards/Message'
